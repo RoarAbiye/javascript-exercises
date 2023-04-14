e8bb7589255dbe04a14f85ff15068b80e6cdf219
robelabiye <rblabiye5@gmail.com>
2023-04-14 14:04:56 +0300

    working on calculaotor, befor changing test file

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2
+}
+
+const subtract = function(num1, num2) {
+  return num1-num2
 };
 
-const subtract = function() {
-	
+const sum = function(...number_list) {
+  return number_list[0].reduce((sum, ele) => {
+    return sum + ele;
+  },0)
 };
 
-const sum = function() {
-	
+const multiply = function(...number_list) {
+  return number_list[0].reduce((sum, ele) => {
+    return sum * ele;
+  })
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+  return Math.pow(num1,num2)
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+  if (num ===0) {
+    return 1;
+  }
 
-const factorial = function() {
-	
+  else {
+    return factorial(num-1) * num;
+  }
 };
-
 // Do not edit below this line
 module.exports = {
   add,
