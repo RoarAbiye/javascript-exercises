168b2e276271308c39e7ffbf8d845f75ea52d1b6
robelabiye <rblabiye5@gmail.com>
2023-04-14 12:52:40 +0300

    solved upto 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(numberList) {
+  let result = 0;
 
+  for (let i = 0; i < numberList.length; i++) {
+    result += numberList[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
