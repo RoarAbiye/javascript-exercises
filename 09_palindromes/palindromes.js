49ddeb3fd00a504e8b5764bdc7ae819e22aed888
robelabiye <rblabiye5@gmail.com>
2023-04-14 15:00:29 +0300

    solved palindromes exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let orgWord = word.replace(/[^a-zA-Z0-9]/gi, "").toLowerCase() ;
 
+  let reverseWord = orgWord.split("").reverse().join("");
+
+  console.log(orgWord, reverseWord);
+  return orgWord === reverseWord;
 };
-
+console.log(palindromes("Racecar!"));
 // Do not edit below this line
 module.exports = palindromes;
