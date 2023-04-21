0655841c57738b0d1a1b3e3cef3dbf6ab17116bb
robelabiye <rblabiye5@gmail.com>
2023-04-21 12:35:41 +0300

    solved fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
 
+  fibList = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    fibList[i] = fibList[i - 1] + fibList[i - 2];
+  }
+  return fibList[num];
 };
-
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
