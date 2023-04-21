cdd3f46f2702f61e69ebd96575e1db1217c6d6ec
robelabiye <rblabiye5@gmail.com>
2023-04-21 12:45:56 +0300

    solved book title getter exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  //loop throgh the object list of books
+  //add to each book to array by selecting title
+  //return array
+  let result = [];
+  books.forEach((book) => result.push(book.title));
+  return result;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
